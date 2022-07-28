c3fc35a96d59a875598fbd056af496faa1144865
Ramiro Algozino <ramiro@sighup.io>
2022-07-28 17:57:11 +0200

    chore(ci): change test image from ubuntu to dvwa
    ubuntu:16.04 docker image was not triggering vulnerabilities anymore,
    so the vulns count was zero, making the test fail.
    
    changing to damn vulnerable web app image that we are sure will always have vulns > 0

--- katalog/tests/harbor/vulns.sh
+++ katalog/tests/harbor/vulns.sh
@@ -10,7 +10,7 @@ load "./../lib/helper"
 @test "[VULNS] Setup" {
     info
     setup(){
-        docker pull ubuntu:16.04
+        docker pull vulnerables/web-dvwa:1.9
         docker login harbor."${EXTERNAL_DNS}" -u admin -p Harbor12345
     }
     run setup
@@ -20,8 +20,8 @@ load "./../lib/helper"
 @test "[VULNS] Deploy insecure image" {
     info
     deploy(){
-        docker tag ubuntu:16.04 harbor."${EXTERNAL_DNS}"/library/ubuntu:16.04
-        docker push harbor."${EXTERNAL_DNS}"/library/ubuntu:16.04
+        docker tag vulnerables/web-dvwa:1.9 harbor."${EXTERNAL_DNS}"/library/web-dvwa:1.9
+        docker push harbor."${EXTERNAL_DNS}"/library/web-dvwa:1.9
     }
     run deploy
     [ "$status" -eq 0 ]
@@ -30,10 +30,10 @@ load "./../lib/helper"
 @test "[VULNS] Check insecure image is in the registry" {
     info
     test(){
-        tag=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/ubuntu/artifacts/16.04/tags" \
+        tag=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/web-dvwa/artifacts/1.9/tags" \
             -H  "accept: application/json" \
-            --user "admin:Harbor12345" --fail | jq -r .[0].name)
-        if [ "${tag}" != "16.04" ]; then return 1; fi
+            --user "admin:Harbor12345" --fail | jq -r '.[0].name')
+        if [ "${tag}" != "1.9" ]; then return 1; fi
     }
     run test
     [ "$status" -eq 0 ]
@@ -44,7 +44,7 @@ load "./../lib/helper"
     test(){
         health=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/1/scanner" \
             -H  "accept: application/json" \
-            --user "admin:Harbor12345" --fail | jq -r .health)
+            --user "admin:Harbor12345" --fail | jq -r '.health')
         if [ "${health}" != "healthy" ]; then return 1; fi
     }
     run test
@@ -56,7 +56,7 @@ load "./../lib/helper"
     test(){
         # Trigger Scan
         echo "#   Trigger the scan" >&3
-        curl -X POST "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/ubuntu/artifacts/16.04/scan" \
+        curl -X POST "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/web-dvwa/artifacts/1.9/scan" \
             -H  "accept: application/json" \
             --user "admin:Harbor12345" --fail
         # Wait for scan
@@ -67,23 +67,25 @@ load "./../lib/helper"
         echo "#   Wait to get the scan report" >&3
         while [[ "${scan_status}" != "Success" ]] && [[ "${retries}" -lt ${mas_retries} ]]
         do
-            scan_status=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/ubuntu/artifacts/16.04?with_scan_overview=true" \
+            scan_status=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/web-dvwa/artifacts/1.9?with_scan_overview=true" \
                 -H  "accept: application/json" \
                 -H 'x-accept-vulnerabilities: application/vnd.security.vulnerability.report; version=1.1' \
                 --user "admin:Harbor12345" --fail | jq -r '.scan_overview["application/vnd.security.vulnerability.report; version=1.1"].scan_status')
+            echo "#     Scan status is: '${scan_status}'" >&3
             if [ "${scan_status}" != "Success" ]; then echo "#     Scan is not ready yet" >&3; let "retries+=1"; sleep ${retry_seconds}; fi
         done
         if [ "${scan_status}" != "Success" ]; then return 1; fi
         # See scan report
         echo "#   Checking scan report" >&3
-        vulns=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/ubuntu/artifacts/16.04?with_scan_overview=true" \
+        vulns=$(curl -s -X GET "https://harbor.${EXTERNAL_DNS}/api/v2.0/projects/library/repositories/web-dvwa/artifacts/1.9?with_scan_overview=true" \
             -H  "accept: application/json" \
-            -H 'x-accept-vulnerabilities: application/vnd.security.vulnerability.report; version=1.1' \
+            -H 'x-accept-vulnerabilities: application/vnd.security.vulnerability.report; version=1.1, application/vnd.scanner.adapter.vuln.report.harbor+json; version=1.0' \
             --user "admin:Harbor12345" --fail | jq -r '.scan_overview["application/vnd.security.vulnerability.report; version=1.1"].summary.total')
+        echo "#     Vulnerabilities is: '${vulns}'" >&3
         if [ "${vulns}" == "null" ]; then echo "#     No vulnerabilities found. Retrying" >&3; return 1; fi
         if [ "${vulns}" -eq "0" ]; then echo "#     No vulnerabilities found. Retrying" >&3; return 1; fi
     }
-    loop_it test 30 60
+    loop_it test 30 30
     status=${loop_it_result}
     [ "$status" -eq 0 ]
 }
